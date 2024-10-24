3acbe672083605f829cf2c161505c490a24cf9a5
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-24 16:15:09 +0530

    Added: routing

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config(); 
 import express from 'express';
-
-import {sendEmail} from './config/mailer';
 const app = express();
-
+import notificationRoute from './routes/notificationRoute';
+const notificationroute = notificationRoute;
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
-
-app.post('/send', (req: express.Request, res: express.Response) => {
-    const {to, subject, text} = req.body;
-    sendEmail(to, subject, text);
-    res.send('Email sent');
-});
+app.use('/api', notificationroute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/routes/notificationRoute.ts
@@ -0,0 +1,23 @@
+
+import express from 'express';
+import { sendEmail } from '../config/mailer';
+const notificationRoute = express.Router();
+
+console.log('notificationRoute.js');
+
+notificationRoute.post('/notification/send', (req: express.Request, res: express.Response) => {
+    const {to, subject, text} = req.body;
+
+    try{
+        sendEmail(to, subject, text);
+        res.send('Email sent');
+    }
+    catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json({ error: 'Failed to send email.' });
+    }
+    
+
+});
+
+export default notificationRoute;
